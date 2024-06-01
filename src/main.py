9bbe2c4deb04ad84804dadf64c23ece06ecb75cc
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-06-01 13:37:11 +0300

    fix db companies.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from config import database, user, password, host
 from db_manager import DBManager
 
+
 def main():
     # Инициализация менеджера базы данных
     db_manager = DBManager(database, user, password, host)
@@ -17,19 +18,22 @@ def main():
         print(f"Список всех компаний и количество вакансий у каждой компании: {companies_vacancies_count}")
     elif user_request == '2':
         vacancy_list = db_manager.get_all_vacancies()
-        print(f"Cписок всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию: {vacancy_list}")
+        print(
+            f"Cписок всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию: {vacancy_list}")
     elif user_request == '3':
         avg_salary = db_manager.get_avg_salary()
         print(f"Средняя зарплату по вакансиям: {avg_salary}")
     elif user_request == '4':
         vacancies_with_higher_salary = db_manager.get_vacancies_with_higher_salary()
-        print(f"Список всех вакансий, у которых зарплата выше средней по всем вакансиям: {vacancies_with_higher_salary}")
+        print(
+            f"Список всех вакансий, у которых зарплата выше средней по всем вакансиям: {vacancies_with_higher_salary}")
     elif user_request == '5':
         user_input = input(f'Введите слово:')
         vacancies_with_keyword = db_manager.get_vacancies_with_keyword(user_input)
         print(f"список всех вакансий, в названии которых содержатся {user_input}: {vacancies_with_keyword}")
     else:
         print(f"Введён неверный запрос")
+
 
 if __name__ == "__main__":
     main()
