e5e962ffea8c7fb68e0185849eb05d6c7aaadb71
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-06-01 08:19:34 +0300

    add db companies.py

--- src/conn_bd.py
+++ src/conn_bd.py
@@ -2,6 +2,11 @@ import json
 import psycopg2
 from config import database, user, password, host
 
+
+# Чтение данных из JSON файла
+with open('companies_id.json') as json_file:
+    data = json.load(json_file)
+
 with psycopg2.connect(
         host=host,
         database=database,
@@ -9,7 +14,22 @@ with psycopg2.connect(
         password=password
 ) as conn:
     with conn.cursor() as cur:
-        # удаляем таблицу, если она уже существует
+        # удаляем таблицу vacancy_table, если она уже существует
+        cur.execute("DROP TABLE IF EXISTS companies")
+        # Создаем таблицу companies, если она не существует
+        cur.execute("CREATE TABLE companies (company_name VARCHAR(255), company_id INT)")
+
+        # Добавляем данные из JSON файла в таблицу
+        for company_name, company_id in data[0].items():
+            cur.execute("INSERT INTO companies (company_name, company_id) VALUES (%s, %s)", (company_name, company_id))
+
+        # Фиксируем изменения
+        conn.commit()
+
+
+
+
+        # удаляем таблицу vacancy_table, если она уже существует
         cur.execute("DROP TABLE IF EXISTS vacancy_table")
         # создаём в postgres sql таблицу вакансий
         cur.execute("""
